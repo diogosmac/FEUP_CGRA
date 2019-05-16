2bd09820e660b960ddb1caf32257523c0987e0e1
EduRibeiro00 <eribeiro306@gmail.com>
2019-05-16 15:55:53 +0100

    Adicionados ficheiros

--- WebCGF2.projB/projB/MyScene.js
+++ WebCGF2.projB/projB/MyScene.js
@@ -21,11 +21,45 @@ class MyScene extends CGFscene {
         this.enableTextures(true);
         this.setUpdatePeriod(50);
 
+        this.initTextures();
+
         //Initialize scene objects
         this.axis = new CGFaxis(this);
         this.plane = new Plane(this, 32);
+        this.cubeMap = new MyCubeMap(this);
+        // this.house = new MyHouse(this, 4,
+        //                          this.brickTexture,
+        //                          this.doorTexture,
+        //                          this.balconyTexture,
+        //                          this.roofTexture,
+        //                          this.houseDetailTexture,
+        //                          this.columnTexture,
+        //                          this.welcomeMatTexture,
+        //                          this.windowTexture);
+
+        this.timeOfDay = 0;
+
+        // this.defaultMaterial = new CGFappearance(this);
+        // this.defaultMaterial.setAmbient(0.6, 0.6, 0.6, 1.0);
+        // this.defaultMaterial.setDiffuse(0.7, 0.7, 0.7, 1.0);
+        // this.defaultMaterial.setSpecular(0.1, 0.1, 0.1, 1.0);
+        // this.defaultMaterial.setShininess(10.0);
+        
 
         //Objects connected to MyInterface
+    }
+    initTextures() {
+        
+        // House
+        this.brickTexture = new CGFtexture(this, 'images/brickWall.jpg');
+        this.doorTexture = new CGFtexture(this, 'images/door.jpg');
+        this.balconyTexture = new CGFtexture(this, 'images/balconyFloor.jpg');
+        this.roofTexture = new CGFtexture(this, 'images/roofTiles.jpg');
+        this.houseDetailTexture = new CGFtexture(this, 'images/rodape.jpg');
+        this.columnTexture = new CGFtexture(this, 'images/marbleColumn.jpg');
+        this.welcomeMatTexture = new CGFtexture(this, 'images/welcomeMat.jpg');
+        this.windowTexture = new CGFtexture(this, 'images/window.jpg');
+
     }
     initLights() {
         this.lights[0].setPosition(15, 2, 5, 1);
@@ -64,11 +98,15 @@ class MyScene extends CGFscene {
         this.setDefaultAppearance();
 
         // ---- BEGIN Primitive drawing section
+
         this.pushMatrix();
         this.rotate(-0.5*Math.PI, 1, 0, 0);
         this.scale(60, 60, 1);
         this.plane.display();
         this.popMatrix();
+
+        this.cubeMap.cubeMapMaterial.apply();
+        this.cubeMap.display();
         // ---- END Primitive drawing section
     }
 }
